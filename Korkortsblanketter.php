4e12c0a38335161b6a9530730efe28736b185ac8
dansim <dana.simola@omegapoint.se>
2015-07-19 23:48:26 +0200

    modified styling of Korkortsblanketter.php

--- Korkortsblanketter.php
+++ Korkortsblanketter.php
@@ -2,48 +2,95 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-		<link rel="stylesheet" href="css/main.css">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link href="css/landing-page.css" rel="stylesheet">
+		<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 		<title>Sveriges Trafikskolor</title>
 	</head>
 	<body>
-		<div id="container">
-			<div id="head">
-				<img src="img/logo.jpg" class="logo" alt="Logo" />
-				<h1 style="float: left;">Sveriges Trafikskolor</h1>
-			</div>
-			<div id="links">
-			<ul id="linkList">
-				<il><a href="index.php">Bil</a></il>
-				<il><a href="Korkortsblanketter.php">Körkortsblanketter</a></il>
+    <a name="about"></a>
+    <div class="intro-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-message">
+                        <h1>Sveriges Trafikskolor</h1>
+                        <h3>En kort sammanfattning</h3>
+                        <hr class="intro-divider">
+                        <ul class="list-inline intro-social-buttons">
+                            <li><a href="index.php" class="btn btn-default btn-lg">Bil</a></li>
+							<li><a href="Korkortsblanketter.php" class="btn btn-default btn-lg">Körkortsblanketter</a></li>
+							<li><a href="syntest.php" class="btn btn-default btn-lg">Syntest</a></li>
+							<li><a href="handledarutbildning.php" class="btn btn-default btn-lg">Handledarutbildning</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
 
-<il><a href="syntest.php">Syntest</a></il>
+        </div>
+    </div>
+    <div class="content-section-a">
+	    <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-sm-12">
+                    <hr class="section-heading-spacer">
+                    <div class="clearfix"></div>
+                    <h2 class="section-heading">Körkortsblanketter</h2>
+                    <p class="lead">
+                    	Det första du bör göra för att få <a class="table2"  href="ovningskora.php">övningsköra </a> är att fylla i vissa blanketter.
+                    </p>
+                </div>
+            </div>
+	    </div>
+	</div>
 
-<il><a href="handledarutbildning.php">Handledarutbildning</a></il>
+	<div class="content-section-b">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-sm-12">
+                    <hr class="section-heading-spacer">
+                    <div class="clearfix"></div>
+                    <h2 class="section-heading">Ska du köra med trafikskola eller privat?</h2>
+                    <p class="lead">
+                    	Ska du bara övningsköra på en trafikskola räcker det med bara en blankett för att ansökan om <a class="table2" href="korkortstillstand.php">körkortstillstånd </a> och hälsodeklarationen (med <a class="table2"  href="syntest.php">syntest </a>).
+						För att få köra med handledare behöver du en handledarblankett samt ett intyg på att eleven och handledaren har undergått en <a class="table2" href="handledarutbildning.php">introduktionsutbildning(handledarutbildning)</a>.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 
-			</ul>
-			</div>
-			<div id="sortAndFilters" style="padding: 10px;">		
-						<h1>Körkortsblanketter</h1>
-Det första du bör göra för att få <a class="table2"  href="ovningskora.php">övningsköra </a>
- är att fylla i vissa blanketter.
-<h2>Ska du köra med trafikskola eller privat? </h2>
-Ska du bara övningsköra på en trafikskola räcker det med bara en blankett för att ansökan om <a class="table2" href="korkortstillstand.php">körkortstillstånd </a> och hälsodeklarationen (med <a class="table2"  href="syntest.php">syntest </a>).
-För att få köra med handledare behöver du en handledarblankett samt ett intyg på att eleven och handledaren har undergått en <a class="table2" href="handledarutbildning.php">introduktionsutbildning(handledarutbildning)</a>.
-<h2 >Ansök direkt via internet</h2>
-
-	<a class="table2" href=" http://www.korkortsportalen.se/e-tjanster/"></b>Transportstyrelsens e-tjänster för körkortstillstånd och handledarskap</b></a>
-Ansökan om körkortstillstånd måste skickas med ett synintyg.  <a class="table2" href=" syntest.php"> Klicka här för mer info om syntest</a>
-Du kan betala avgiften med kort eller internetbank samtidigt du skickar in ansökan. I annars får du hem ett inbetalningskort.
-
-
-
-
-
-			</div>
-			<!-- Dynamic content - End -->
-			<div id="footer"></div>
-		</div>
+     <div class="content-section-a">
+	    <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-sm-12">
+                    <hr class="section-heading-spacer">
+                    <div class="clearfix"></div>
+                    <h2 class="section-heading">Ansök direkt via internet</h2>
+                    <p class="lead">
+                    	<a class="table2" href=" http://www.korkortsportalen.se/e-tjanster/"></b>Transportstyrelsens e-tjänster för körkortstillstånd och handledarskap</b></a>
+						Ansökan om körkortstillstånd måste skickas med ett synintyg.  <a class="table2" href=" syntest.php"> Klicka här för mer info om syntest</a>
+						Du kan betala avgiften med kort eller internetbank samtidigt du skickar in ansökan. I annars får du hem ett inbetalningskort.
+                    </p>
+                </div>
+            </div>
+	    </div>
+	</div>
+	 <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                	<hr>
+                    <p class="copyright text-muted small">Partner <a class="table3" href="http://makeup.ph">http://makeup.ph</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
 	</body>
 </html>
